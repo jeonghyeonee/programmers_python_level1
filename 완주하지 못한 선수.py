e52ce1ef6f495eff27bfc5235db859ff2b443bf8
jeonghyeonee <jlh1218@naver.com>
2021-10-20 10:39:54 +0900

    2021.10.19.

--- 완주하지 못한 선수.py
+++ 완주하지 못한 선수.py
@@ -1,7 +1,34 @@
 def solution(participant, completion):
-    for i in completion:
-        if i in participant:
-            participant.remove(i)
-    for i in participant:
-        answer = i
-    return answer
+    # for i in completion:
+    #     if i in participant:
+    #         participant.remove(i)
+    # for i in part이ipant:
+    #     answer = i
+    # return answer
+
+    completion.sort()
+    participant.sort()
+    
+    for idx, p in enumerate(participant):
+        if p != completion[idx]:
+            return p
+
+# 효율성 통과 못 한 이유
+# remove, if -> O(n^2)
+
+# 다른 사람 풀이
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for p,c in zip(participant, completion):
+        if p != c:
+            return p
+    return participant.pop()
+
+# 다른 사람 풀이
+import collections
+
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
