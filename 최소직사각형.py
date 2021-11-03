97d28275a06113d62e69a8fdb32305c1924152d0
jeonghyeonee <jlh1218@naver.com>
2021-11-03 18:11:23 +0900

    2021.11.03. 최소 직사각형

--- 최소직사각형.py
+++ 최소직사각형.py
@@ -1,3 +1,19 @@
 def solution(sizes):
-    answer = 0
-    return answer
+    w = []
+    h = []
+    for i in range(len(sizes)):
+        if sizes[i][0] >= sizes[i][1]:
+            w.append(sizes[i][0])
+            h.append(sizes[i][1])
+        else:
+            h.append(sizes[i][0])
+            w.append(sizes[i][1])
+
+    return max(w) * max(h)
+
+# 다른 사람 풀이
+def solution(sizes):
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
+
+# w, h 중 큰 값을 모음
+# 그 중 큰 값, w, h 중 작은 값을 모아서 그 중 큰 값을 곱함
