12878229d1445b4fbf7d78341b6c49d0d91af2aa
jeonghyeonee <jlh1218@naver.com>
2021-11-15 18:48:50 +0900

    2021.11.15. 2016년 문제 해결

--- 2016년.py
+++ 2016년.py
@@ -1,3 +1,6 @@
 def solution(a, b):
-    answer = ''
+    day = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
+    month = [31, 29, 31, 30, 31, 30 , 31, 31, 30, 31, 30, 31]
+    d = sum(month[:a-1]) + b
+    answer = day[d % 7]
     return answer
