4c4496507279ab6c3b88b25035a6facbf65d8698
jeonghyeonee <jlh1218@naver.com>
2021-11-22 15:44:35 +0900

    2021.11.21. 나누어 떨어지는 숫자 배열

--- 나누어떨어지는숫자배열.py
+++ 나누어떨어지는숫자배열.py
@@ -1,3 +1,9 @@
 def solution(arr, divisor):
     answer = []
+    for i in range(len(arr)):
+        if arr[i]%divisor == 0:
+            answer.append(arr[i])
+    if len(arr) == 0:
+        answer.append(-1)
+    answer.sort()
     return answer
