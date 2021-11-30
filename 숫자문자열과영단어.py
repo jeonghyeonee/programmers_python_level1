171e3624f33479fd166649cbe327e59347a883d8
jeonghyeonee <jlh1218@naver.com>
2021-11-30 21:55:34 +0900

    2021.11.29. 숫자 문자열과 영단어

--- 숫자문자열과영단어.py
+++ 숫자문자열과영단어.py
@@ -1,5 +1,10 @@
 # 어떻게 풀어야 하지?
 
 def solution(s):
-    answer = 0
-    return answer
+    dic = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    answer = s
+    
+    for i in dic.items():
+        answer = answer.replace(i[0], str(i[1]))
+    
+    return int(answer)
