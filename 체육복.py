ca807a1f86dd9e2c000f2bb5742ab1d2e697515a
jeonghyeonee <jlh1218@naver.com>
2021-12-02 23:07:03 +0900

    2021.12.01. 체육복

--- 체육복.py
+++ 체육복.py
@@ -1,5 +1,14 @@
 # 단순하게 n에서 lost빼고 reserve 더하는 방식으로 풀어보려고 하지만, 안되는 케이스 존재
+# https://jokerldg.github.io/algorithm/2021/04/03/gym-suit.html
 
 def solution(n, lost, reserve):
-    answer = 0
-    return answer
+    reserve_set = set(reserve)-set(lost) 
+    lost_set = set(lost)-set(reserve) 
+    
+    for reserve_person in reserve_set: 
+        if reserve_person-1 in lost_set: 
+            lost_set.remove(reserve_person-1) 
+        elif reserve_person+1 in lost_set: 
+            lost_set.remove(reserve_person+1) 
+    
+    return n-len(lost_set)
