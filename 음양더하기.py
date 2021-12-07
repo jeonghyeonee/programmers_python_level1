21e58daab4a64ef0eb38c3a7d1c53813874528a8
jeonghyeonee <jlh1218@naver.com>
2021-12-07 22:25:01 +0900

    2021.12.07. 음양더하기

--- 음양더하기.py
+++ 음양더하기.py
@@ -1,3 +1,10 @@
+# 파이썬에서는 "true"가 아니라 True로 입력해주어야 함!
+
 def solution(absolutes, signs):
-    answer = 123456789
+    answer = 0
+    for i in range(len(signs)):
+        if signs[i] == True:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
     return answer
