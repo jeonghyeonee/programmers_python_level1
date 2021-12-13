5c759a41842acfa99a96ad9a70fbcfbc1da9bedf
jeonghyeonee <jlh1218@naver.com>
2021-12-13 21:24:02 +0900

    2021.12.13. 소수만들기

--- 소수만들기.py
+++ 소수만들기.py
@@ -1,7 +1,16 @@
+# https://eda-ai-lab.tistory.com/493 
+
+from itertools import combinations 
+
+def check(a, b, c): 
+    total = a + b + c
+    for i in range(2, total): 
+        if total % i == 0 : return False 
+    return True 
+
 def solution(nums):
-    answer = -1
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
+    answer = 0
+    A = list(combinations(nums, 3))
+    for i in A: 
+        if check(i[0], i[1], i[2]): answer += 1
     return answer
