c4a064afd0459556e80cf5b57613660bdc805597
jeonghyeonee <jlh1218@naver.com>
2021-12-15 22:38:06 +0900

    2021.12.15.

--- 실패율.py
+++ 실패율.py
@@ -1,3 +1,8 @@
+# 실패율 = 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+# N = 전체 스테이지의 개수, stages = 현재 멈춰있는 스테이지의 번호
+# 실패율이 높은 스테이지부터 내림차순으로 스테이지 번호 정렬 return
+# dictionary 쓰면 풀 수 있을 것 같은데...
+
 def solution(N, stages):
     answer = []
     return answer
