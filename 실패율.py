130240e615bb38a0f77aca5626e25acbb6f42830
jeonghyeonee <jlh1218@naver.com>
2021-12-18 22:20:04 +0900

    2021.12.18. 실패율

--- 실패율.py
+++ 실패율.py
@@ -3,8 +3,15 @@
 # 실패율이 높은 스테이지부터 내림차순으로 스테이지 번호 정렬 return
 # dictionary 쓰면 풀 수 있을 것 같은데...
 
-# 좀 더 고민좀..
-# 종강했는데,,, 풀기 싫다,,,어쩜 좋아...
 def solution(N, stages):
-    answer = []
-    return answer
+    answer = {}
+    l = len(stages)
+    for i in range(1, N+1):
+        if l != 0:
+            cnt = stages.count(i)
+            answer[i] = cnt / l
+            l -= cnt
+        else:
+            answer[i] = 0
+    result = sorted(answer, key=lambda x : answer[x], reverse=True)
+    return result
