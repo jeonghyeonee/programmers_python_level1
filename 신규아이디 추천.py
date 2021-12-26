df981969d442621d7b27f6915c6853027ccaebad
jeonghyeonee <jlh1218@naver.com>
2021-12-26 20:26:42 +0900

    2021.12.26.

--- 신규아이디 추천.py
+++ 신규아이디 추천.py
@@ -1,5 +1,10 @@
 # 문제를 차근차근 이해해보자 
 
 def solution(new_id):
+    l = len(new_id)
     answer = ''
+    new_id = string.lower(new_id)
+    if l>=16:
+        answer = new_id[:14]
+        
     return answer
