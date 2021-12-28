761b89697af8c1b83d661521cd48d035f13ea814
jeonghyeonee <jlh1218@naver.com>
2021-12-28 21:08:43 +0900

    2021.12.28.

--- 신규아이디 추천.py
+++ 신규아이디 추천.py
@@ -1,10 +1,31 @@
  # 어떻게 풀어야하지...
  
 def solution(new_id):
-    l = len(new_id)
     answer = ''
-    new_id = string.lower(new_id)
-    if l>=16:
-        answer = new_id[:14]
+    new_id = new_id.lower()
+    
+    for i in new_id:
+        if i.isalpha() or i.isdigit() or i in ['-', '_', '.']:
+            answer += i
+    
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    
+    if answer[0] == '.':
+        answer = answer[1:] if len(answer) > 1 else '.'
+    
+    if answer[-1] == '.':
+        answer = answer[:-1]
+    
+    if answer == '':
+        answer = 'a'
+    
+    if len(answer) > 15:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    
+    while len(answer) < 3:
+        answer += answer[-1]
         
     return answer
