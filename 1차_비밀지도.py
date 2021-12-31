9f52d4250546d10c1b1cd67b6ea055210eadc75a
jeonghyeonee <jlh1218@naver.com>
2021-12-31 16:26:20 +0900

    2021.12.31.

--- 1차_비밀지도.py
+++ 1차_비밀지도.py
@@ -1,5 +1,11 @@
+# https://jokerldg.github.io/algorithm/2021/04/23/secret-map.html
+
 def solution(n, arr1, arr2):
     answer = []
-    for i in range(n):
-        arr[i] = arr
+    for i, j in zip(arr1, arr2):
+        value = str(bin(i|j)[2:])
+        value = value.zfill(n)
+        value = value.replace('1', '#')
+        value = value.replace('0', ' ')
+        answer.append(value)
     return answer
