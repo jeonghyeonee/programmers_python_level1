94bdfb26a70f1cf22e0b4166e93e8cfd456fda9e
jeonghyeonee <jlh1218@naver.com>
2022-01-03 21:37:47 +0900

    2022.01.03.

--- 크레인인형뽑기.py
+++ 크레인인형뽑기.py
@@ -1,3 +1,17 @@
 def solution(board, moves):
     answer = 0
+    list = []
+    
+    for i in moves:
+        for j in board:
+            if j[i-1] != 0:
+                list.append(j[i-1])
+                
+                if len(list) > 1:
+                    if list[-2] == list[-1]:
+                        del list[-2]
+                        del list[-1]
+                        answer += 2
+                j[i-1] = 0
+                break
     return answer
